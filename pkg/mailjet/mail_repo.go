7851918843b9951c6a3053f6eeadebf27d109040
Andreas Hansson <andreas.olof.hansson@gmail.com>
2021-11-19 15:47:43 +0100

    Don't use fnutts in your .env file. They will use them as part of the string

--- pkg/mailjet/mail_repo.go
+++ pkg/mailjet/mail_repo.go
@@ -11,11 +11,7 @@ type MailRepository struct {
 }
 
 func NewMailRepo(c *config.MailjetConfig) *MailRepository {
-	key1 := c.Key1
-	key2 := c.Key2
-	key1 = "e7607096ff679c66e4b8fd1a8bac9766"
-	key2 = "95b213f8754a237524cb8a747ffc7cab"
-	client := mailjet.NewMailjetClient(key1, key2)
+	client := mailjet.NewMailjetClient(c.Key1, c.Key2)
 	return &MailRepository{
 		Client: client,
 	}
