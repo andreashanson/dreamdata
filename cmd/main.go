4f43ae4de146931f2d51064a39ae0547d313b702
Andreas Hansson <andreas.olof.hansson@gmail.com>
2021-12-01 08:21:56 +0100

    Moved more stuff to main

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,33 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/andreashanson/dreamdata/pkg/config"
+	"github.com/andreashanson/dreamdata/pkg/handlers"
+	"github.com/andreashanson/dreamdata/pkg/mail"
+	"github.com/andreashanson/dreamdata/pkg/mailjet"
 	"github.com/andreashanson/dreamdata/pkg/routes"
+	"github.com/andreashanson/dreamdata/pkg/sendinblue"
 )
 
 func main() {
 
+	cfg := config.NewConfig()
+	mailjetConfig := cfg.MailjetConfig
+	sendinblueConfig := cfg.SMTPConfig
+
+	mailjetRepo := mailjet.NewMailjetRepo(mailjetConfig)
+	mailSrv1 := mail.NewService(mailjetRepo)
+
+	sendinblueRepo := sendinblue.NewSendinBlueRepo(sendinblueConfig)
+	mailSrv2 := mail.NewService(sendinblueRepo)
+
+	handlerRepo := handlers.NewHandlersRepo(mailSrv1, mailSrv2)
+
+	newRoutes := routes.GetRoutes(handlerRepo)
+
 	srv := &http.Server{
 		Addr:    ":8000",
-		Handler: routes.GetRoutes(),
+		Handler: newRoutes,
 	}
 
 	err := srv.ListenAndServe()
--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -3,29 +3,14 @@ package routes
 import (
 	"net/http"
 
-	"github.com/andreashanson/dreamdata/pkg/config"
 	"github.com/andreashanson/dreamdata/pkg/handlers"
-	"github.com/andreashanson/dreamdata/pkg/mail"
-	"github.com/andreashanson/dreamdata/pkg/mailjet"
-	"github.com/andreashanson/dreamdata/pkg/sendinblue"
 	"github.com/go-chi/chi"
 )
 
-func GetRoutes() http.Handler {
-	cfg := config.NewConfig()
-	mailjetConfig := cfg.MailjetConfig
-	sendinblueConfig := cfg.SMTPConfig
-
-	mailjetRepo := mailjet.NewMailjetRepo(mailjetConfig)
-	mailSrv1 := mail.NewService(mailjetRepo)
-
-	sendinblueRepo := sendinblue.NewSendinBlueRepo(sendinblueConfig)
-	mailSrv2 := mail.NewService(sendinblueRepo)
-
-	handlerRepo := handlers.NewHandlersRepo(mailSrv1, mailSrv2)
+func GetRoutes(h *handlers.HandlersRepo) http.Handler {
 
 	r := chi.NewRouter()
-	r.Post("/mail/send", http.HandlerFunc(handlerRepo.SendMailHandler))
+	r.Post("/mail/send", http.HandlerFunc(h.SendMailHandler))
 	r.Get("/*", http.HandlerFunc(handlers.ServeReactApp))
 	return r
 }
